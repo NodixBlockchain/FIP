bfced1f285c8f7ff94a7e51b43760f489f9748ee
NodixBlockchain <a.bentue-ferrer@nodix.eu>
2020-01-22 17:01:00 +0100

    car

--- CI-App/app/Views/cat_review1.php
+++ CI-App/app/Views/cat_review1.php
@@ -257,16 +257,24 @@
 			<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
 			  <div class="carousel-inner">
 				<div class="col text-center">
-					<?php foreach($pictures as $picture) { ?>
-						<div class="carousel-item">
+					<?php foreach($pictures as $picture) { $active = 'active'; ?>
+						<div class="carousel-item <?= $active ?>">
 						<img class="d-block w-100" src="<?= $picture['url'] ?>" alt="cat picture" />
 						<form action="<?= site_url('/Cat/del_tmp_pic/'.$catHash.'/'.$picture['file']) ?>" method="POST">
 							<input type="submit" value="del" class="btn btn-primary" />
 						</form>
 						</div>
-					<?php } ?>
+					<?php $active = ''; } ?>
 				</div>
 			 </div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			  </a>
 			</div>
 
 			</div>
