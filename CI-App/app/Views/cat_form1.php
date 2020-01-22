414aad353764dae7270d1fb9d4cb5de2a2ba8a53
NodixBlockchain <a.bentue-ferrer@nodix.eu>
2020-01-22 09:30:06 +0100

    update form

--- CI-App/app/Views/cat_form1.php
+++ CI-App/app/Views/cat_form1.php
@@ -359,23 +359,28 @@
 					</div>
 
 					<div class="jumbotron">
-						<h2>FIP form</h2>
-						<div class="fip-type" id="wet-form">
 						
-							<h4>wet form 5ml/kg</h4>
-							<div id="effusion-block" <?php if((!array_key_exists('symptoms-FIP',$symptoms))||(array_search('wet',$symptoms['symptoms-FIP'])===FALSE)){ echo 'class="hidden"'; } ?>>
-								<h3>Effusion type</h3>
-								<div class="form-group">
-									<div class="checkbox"><label><input type="checkbox" name="symptoms-effusion[]" id="symptoms-effusion-abdominal"	  value="abdominal"		<?php if((array_key_exists('symptoms-effusion',$symptoms))&&(array_search('abdominal',$symptoms['symptoms-effusion'])!==FALSE)){ echo 'checked'; } ?> >abdominal</label></div>
-									<div class="checkbox"><label><input type="checkbox" name="symptoms-effusion[]" id="symptoms-effusion-chest"		  value="chest"			<?php if((array_key_exists('symptoms-effusion',$symptoms))&&(array_search('chest',$symptoms['symptoms-effusion'])!==FALSE)){ echo 'checked'; } ?> >chest</label></div>
-									<div class="checkbox"><label><input type="checkbox" name="symptoms-effusion[]" id="symptoms-effusion-pericardial" value="pericardial"	<?php if((array_key_exists('symptoms-effusion',$symptoms))&&(array_search('pericardial',$symptoms['symptoms-effusion'])!==FALSE)){ echo 'checked'; } ?> >pericardial</label></div>
+						<h2>FIP form</h2>
+
+						<div class="row">
+						<div class="col">
+							<div class="fip-type" id="wet-form">
+						
+								<h4>wet form 5ml/kg</h4>
+								<div id="effusion-block" <?php if((!array_key_exists('symptoms-FIP',$symptoms))||(array_search('wet',$symptoms['symptoms-FIP'])===FALSE)){ echo 'class="hidden"'; } ?>>
+									<h3>Effusion type</h3>
+									<div class="form-group">
+										<div class="checkbox"><label><input type="checkbox" name="symptoms-effusion[]" id="symptoms-effusion-abdominal"	  value="abdominal"		<?php if((array_key_exists('symptoms-effusion',$symptoms))&&(array_search('abdominal',$symptoms['symptoms-effusion'])!==FALSE)){ echo 'checked'; } ?> >abdominal</label></div>
+										<div class="checkbox"><label><input type="checkbox" name="symptoms-effusion[]" id="symptoms-effusion-chest"		  value="chest"			<?php if((array_key_exists('symptoms-effusion',$symptoms))&&(array_search('chest',$symptoms['symptoms-effusion'])!==FALSE)){ echo 'checked'; } ?> >chest</label></div>
+										<div class="checkbox"><label><input type="checkbox" name="symptoms-effusion[]" id="symptoms-effusion-pericardial" value="pericardial"	<?php if((array_key_exists('symptoms-effusion',$symptoms))&&(array_search('pericardial',$symptoms['symptoms-effusion'])!==FALSE)){ echo 'checked'; } ?> >pericardial</label></div>
 								
-									<span class="bmd-help">For wet fip, select the type of effusion.</span>
+										<span class="bmd-help">For wet fip, select the type of effusion.</span>
+									</div>
 								</div>
 							</div>
 						</div>
-						<div class="fip-type" id="neuro-form"><h4>neuro form 10ml/kg</h4></div>
-						<div class="fip-type" id="ocular-form"><h4>ocular form 8ml/kg</h4></div>
+						<div class="row"><div class="col"><div class="fip-type" id="neuro-form"><h4>neuro form 10ml/kg</h4></div></div>
+						<div class="row"><div class="col"><div class="fip-type" id="ocular-form"><h4>ocular form 8ml/kg</h4></div></div>
 					</div>
 
 
